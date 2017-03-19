57141a2b2f1a6afbc5161b56e806d4702495cb93
kalaider <a.kalaider@yandex.ru>
2017-03-19 22:58:17 +0300

    An adequate initialization of USART and PWM added

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,18 @@
 #include "common.h"
 
+#define IBUF_SIZE    16
+#define OBUF_SIZE    32
+#define BAUD_RATE    9600
+#define FOSC         1000000
+#define TC2_PRESCALE 1
+
+#include "usart.h"
+#include "pwm.h"
+
 int main()
 {
+    iobuf_state = IBUF_NFULL | OBUF_NFULL;
+    usart_init();
+    init_tc2_wg();
     return 0;
 }
