f9250cee4ac0a000f286f4b2a887e54be81c22e6
kalaider <a.kalaider@yandex.ru>
2017-03-25 21:35:35 +0300

    Unused pins configured to be internally pulled up

--- src/main.cpp
+++ src/main.cpp
@@ -23,6 +23,26 @@ inline __monitor void init()
        1st channel, internal Vref, left alignment) */
     REMUX(0);
     ADCSR = (1<<ADEN)|(3<<ADPS0);
+    
+    /* Pin configuration */
+    PORTB = (1<<PORTB0)|(1<<PORTB1)|(1<<PORTB2)|
+            (0<<PORTB3)|(1<<PORTB4)|(1<<PORTB5)|
+            (1<<PORTB6)|(1<<PORTB7);
+    DDRB = (0<<DDB0)|(0<<DDB1)|(0<<DDB2)|
+           (1<<DDB3)|(0<<DDB4)|(0<<DDB5)|
+           (0<<DDB6)|(0<<DDB7);
+    PORTC = (0<<PORTC0)|(0<<PORTC1)|(1<<PORTC2)|
+            (1<<PORTC3)|(1<<PORTC4)|(1<<PORTC5)|
+            (1<<PORTC6);
+    DDRC = (0<<DDC0)|(0<<DDC1)|(0<<DDC2)|
+           (0<<DDC3)|(0<<DDC4)|(0<<DDC5)|
+           (0<<DDC6);
+    PORTD = (0<<PORTD0)|(0<<PORTD1)|(1<<PORTD2)|
+            (1<<PORTD3)|(1<<PORTD4)|(1<<PORTD5)|
+            (1<<PORTD6)|(1<<PORTD7);
+    DDRD = (0<<DDD0)|(1<<DDD1)|(0<<DDD2)|
+           (0<<DDD3)|(0<<DDD4)|(0<<DDD5)|
+           (0<<DDD6)|(0<<DDD7);
 }
     
     
